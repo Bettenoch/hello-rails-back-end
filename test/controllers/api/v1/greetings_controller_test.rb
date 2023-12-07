f5e4aa3203da0f01bde0f080334653386e6da09e
Bettenoch <bettenoch254@gmail.com>
2023-12-07 09:26:26 +0300

    Correct linters errors

--- test/controllers/api/v1/greetings_controller_test.rb
+++ test/controllers/api/v1/greetings_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class Api::V1::GreetingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get random_greeting" do
+  test 'should get random_greeting' do
     get api_v1_greetings_random_greeting_url
     assert_response :success
   end
